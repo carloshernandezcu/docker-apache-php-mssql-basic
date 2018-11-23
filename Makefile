95e28b3ed41903ac8a7891e451a26a42b7b12b95
Carlos Hernandez <carlos.hernandez@giffits.de>
2018-11-23 21:18:58 +0000

    Add SQL example

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ help:
 	@echo "Options:"
 	@echo " dev - Start all containers for development. Codebase is shared to containers and debug is enabled."
 	@echo " prod - Start all containers for production. Codebase is copied inside containers."
-	@echo " clean - Stop all containers."
+	@echo " down - Stop and remove all containers."
 
 up:
 	@echo "Starting containers..."
@@ -25,9 +25,9 @@ prod: up
 		up -d --build
 	@echo "URL http://<server>"
 
-clean:
+down:
 	@echo "Stoping and removing containers..."
 	cd $(ROOT_DOCKER) && \
 	docker-compose down
 
-.PHONY: dev prod help
+.PHONY: help dev prod down
--- src/index.php
+++ src/index.php
@@ -6,10 +6,24 @@ echo '<h3>' . $sTitle . '</h3>';
 
 
 # Display enviroment
-$sEnvProd = getenv('PRODUCTION');
-$sEnv = empty($sEnvProd) ? 'dev' : 'prod';
-echo '<p>env ' . $sEnv . ' ' . rand() . '</p>';
+$sEnvProd = getenv('APP_ENV');
+$sEnv = $sEnvProd === 'prod' ? 'Production' : 'Development';
+echo '<p>Environment: ' . $sEnv . ' ' . rand() . '</p>';
 
+# Test MSSQL Connection
+$sServerName = "gifham015";
+$aConnectionInfo = array(
+    "Database" => "Giffits-Extra_Test",
+    "UID" => "sa",
+    "PWD" => ""); // Set password here
+$oConnection = sqlsrv_connect($sServerName, $aConnectionInfo);
+
+if ($oConnection) {
+    echo "<p style='color:green'>OK! Connection established.</p>";
+} else {
+    echo "<p style='color:red'>Error! Connection could not be established.</p>";
+    die(print_r(sqlsrv_errors(), true));
+}
 
 # Some code
-echo 'Hello Giffits 2018 © All rights reserved.';
+echo '<p>Hello Giffits 2019 © All rights reserved.</p>';
